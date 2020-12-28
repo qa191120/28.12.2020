e1963d81825db6e0ff7d7365dd7f4848fa406024
qa191120 <74743291+qa191120@users.noreply.github.com>
2020-12-28 20:48:55 +0200

    Create home_work.java

---
+++ home_work.java
@@ -0,0 +1,28 @@
+         home work:
+         1. input array size from user
+            create int array in the given size
+            populate the array with random numbers
+            sum the array
+            print the average number
+         2. create an array of 5 Strings (hint: String[] word = new String[5])
+            create a for loop and input words from the user into the array
+            now print the array in a for loop
+            *** etgar: print the average length of the words, i.e.  hello java book pro about. the words length is  [5,4,4,3,5] => average=4.2
+         3. create int array A with random numbers (size of 5)  
+            create int array B with random numbers (size of 5)
+            create int array C (size of 5) which each element will be the sum of A+B
+            for exmaple: 
+            A [ 5, 8, 6, 2, 3]  -- random
+            B [ 3, 7, 8, 3, 1]  -- random
+            C [ 8,15,14, 5, 4]  -- sum
+            *etgar: create int array D which will contain the larger name
+            *D[ 5, 8, 8, 3, 3]
+            *etgar: create in array E which will be concat of the array
+            *E[ 5, 8, 6, 2, 3, 3, 7, 8, 3, 1]
+         3. ***etgar crazy:
+            input number of classes from user
+            for each size input number of students
+            input all numbers from user
+            calculate the average of each class in an array
+            calculate the average of averages
+            
